8fedcb9109ce0e50f2dfa98b4ef48c01ed5e7f21
Dadu Khan <photoshopt65@gmail.com>
2026-04-25 16:14:41 +0500

    deleted extra routes and components

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -4,9 +4,6 @@ import { Route, Routes } from "react-router";
 import SellerTable from "../admin/Seller/SellerTable";
 import Coupon from "../admin/Coupon/Coupon";
 import CouponForm from "../admin/Coupon/CouponForm";
-import ElectronicsTable from "../admin/Homepage/ElectronicsTable";
-import ShopByCategory from "../admin/Homepage/ShopByCategory";
-import Deal from "../admin/Deal/Deal";
 import CategoryManager from "../admin/Category/CategoryManager";
 import HomeManager from "../admin/Homepage/HomeManager";
 import ProtectedRoute from "./ProtectedRoute";
@@ -24,12 +21,6 @@ const AdminRoutes = () => {
                 <Route path='/coupons' element={<Coupon />} />
                 <Route path='/add-coupon' element={<CouponForm />} />
                 <Route path='/home-page' element={<HomeManager />} />
-                <Route
-                  path='/electronics-category'
-                  element={<ElectronicsTable />}
-                />
-                <Route path='/shop-by-category' element={<ShopByCategory />} />
-                <Route path='/deals' element={<Deal />} />
                 <Route
                   path='/category-management'
                   element={<CategoryManager />}
